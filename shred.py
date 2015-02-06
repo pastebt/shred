d608f9b1a5d1cd198b48e5afe1145efc33d01c1f
pastebt <maye1968@gmail.com>
2015-02-06 14:40:09 -0800

    add -n, finish all func

--- shred.py
+++ shred.py
@@ -6,14 +6,14 @@ from getopt import gnu_getopt, GetoptError
 
 
 class SRC(object):
-    def __init__(self, s=1024):
-        self.s = 1024
+    def __init__(self, s=10240):
+        self.s = s
 
     def __len__(self):
         return self.s
 
     def __call__(self, sz=None):
-        self.gen(sz if sz else self.s)
+        return self.gen(sz if sz else self.s)
 
     def gen(self, size):
         pass
@@ -21,12 +21,12 @@ class SRC(object):
 
 class RAND(SRC):
     def gen(self, size):
-        return "a" * size
+        return ("a" * size).encode("utf8")
 
 
 class ZERO(SRC):
     def gen(self, size):
-        return "0" * size
+        return ("0" * size).encode("utf8")
 
 
 def do_one(fn, src, verbose):
@@ -38,35 +38,47 @@ def do_one(fn, src, verbose):
             fout.write(src())
             left = left - len(src)
             if verbose:
-                print("%02.2f%%\r" % (size - left) * 100.0 / size)
+                sys.stdout.write("%02.2f%%\r" % ((size - left) * 100.0 / size))
         if left:
             fout.write(src(left))
+        if verbose:
+            print("100.0%")
 
 
-def run(fn, unlink, set_zero, verbose):
-    for i in range(4):
+def run(fn, num, unlink, set_zero, verbose):
+    for i in range(num):
+        if verbose:
+            print("Overwrite time %d:" % (i + 1))
         do_one(fn, RAND(), verbose)
     if set_zero:
+        if verbose:
+            print("Overwrite with zero")
         do_one(fn, ZERO(), verbose)
     if unlink:
+        if verbose:
+            print("Remove file")
         os.unlink(fn)
 
 
 def usage():
-    print("Usage: " + sys.argv[0] + " [-v|-u|-z] filename")
+    print("Usage: " + sys.argv[0] + " [-v|-u|-z|] [-n N]filename")
+    print("     -n N overwrite N times instead of the default (3)")
+    print("     -u   truncate and remove file after overwriting")
+    print("     -v   show progress")
+    print("     -z   add a final overwrite with zeros to hide shredding")
     sys.exit(1)
 
 
 def main():
     try:
-        opts, args = gnu_getopt(sys.argv[1:], "vuz")
+        opts, args = gnu_getopt(sys.argv[1:], "vuzn:")
     except GetoptError as ge:
         print(ge)
         usage()
     if len(args) != 1:
         usage()
     om = dict(opts)
-    run(args[0], "-u" in om, "-z" in om, "-v" in om)
+    run(args[0], int(om.get("-n", 3)), "-u" in om, "-z" in om, "-v" in om)
 
 
 if __name__ == '__main__':
